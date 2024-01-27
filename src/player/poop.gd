c274ac968025c010df83498e07c220ea9e265c03
tehelka-gamedev <17590418+tehelka-gamedev@users.noreply.github.com>
2024-01-27 15:58:20 +0100

    fix poop laughing

--- src/player/poop.gd
+++ src/player/poop.gd
@@ -6,7 +6,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	walk_detection_area.area_entered.connect(_on_walk_on_poop)
+	walk_detection_area.body_entered.connect(_on_walk_on_poop)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,16 +15,16 @@ func _process(delta):
 
 
 # When walk on poop, detect pnj in detection area
-func _on_walk_on_poop(area:Area2D):
-	print("area %s is in group poop : %s" % [area, area.owner.is_in_group(Const.GROUP_WALK_ON_POOP)])
-	if !area.owner.is_in_group(Const.GROUP_WALK_ON_POOP):
+func _on_walk_on_poop(body:Node2D):
+	if !body.is_in_group(Const.GROUP_WALK_ON_POOP):
 		return
 	
 	var pnj_nearby:Array[Node2D] = pnj_detection_area.get_overlapping_bodies()
 	print("PNJ nearby: %s" % [len(pnj_nearby)])
-	for pnj_area in pnj_nearby:
-		var pnj = pnj_area.owner
-		print(pnj)
-		pnj.start_laughing()
+	for pnj_body in pnj_nearby:
+		if body == pnj_body:
+			continue
+		
+		pnj_body.start_laughing()
 	
 	queue_free()
