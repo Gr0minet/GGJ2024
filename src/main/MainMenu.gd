bfbede47613b946ca046281cf3f27a48c332cb71
tehelka-gamedev <17590418+tehelka-gamedev@users.noreply.github.com>
2024-01-28 11:48:43 +0100

    fix circular dependency hell

--- src/main/MainMenu.gd
+++ src/main/MainMenu.gd
@@ -3,18 +3,21 @@ extends Control
 @export var button_play:Button = null
 @export var button_credits:Button = null
 @export var button_quit:Button = null
-
-@export var game_scene:PackedScene = null
+@export var button_quit_credit:Button = null
+@export var credit_panel:PanelContainer = null
+@export_file("*.tscn","*.scn") var game_scene_path:String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	button_play.pressed.connect(func():
-		get_tree().change_scene_to_packed(game_scene)
+		get_tree().change_scene_to_file(game_scene_path)
 	)
 	
 	button_credits.pressed.connect(func():
-		pass
+		credit_panel.show()
 	)
+	
+	button_quit_credit.pressed.connect(_on_quit_credit)
 	
 	# remove quit button if Web (not needed)
 	if OS.get_name() == "Web":
@@ -27,3 +30,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_quit_credit() -> void:
+	credit_panel.hide()
